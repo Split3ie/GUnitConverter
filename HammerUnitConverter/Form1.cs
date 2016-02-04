b4c7703eefa7811f312eb7f2ce6ad0cf5289fa0d
bawdyq <serega.dis@hotmail.com>
2016-02-04 13:53:21 +0200

    Added inch&foot dimensions, some changes in design and logic programm

--- HammerUnitConverter/Form1.cs
+++ HammerUnitConverter/Form1.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace HammerTools
+namespace HammerUnitsConverter
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            //linkLabel1.Links.Add(7,6, "steamcommunity.com/id/bawdyq/");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -16,16 +17,20 @@ namespace HammerTools
             if (textBox1.Text == "")
             {
                 label1.Text = "Input a cm";
+                label2.Text = "";
                 return;
             }
-            double m;
-            if (Double.TryParse(textBox1.Text, out m))
+            double cm;
+            if (Double.TryParse(textBox1.Text, out cm))
             {
-                double unit = (m * 10) / 19.05;
-                label1.Text = string.Format("{0:0.####} units", unit);
-                label2.Text = string.Format("{0} mm = {1:0.#######} untis", m * 10, unit);
-                double unit2 = (m / 100) * 53.333;
-                label3.Text = string.Format("{0} m = {1:0.#####} units", m / 100, unit2);
+                double unit = cm / 19.05;
+                label1.Text = string.Format("{0:0.##} units", unit*10);
+                double inch = cm * 0.39370079;
+                double foot = inch / 12;
+                label2.Text = string.Format(@"{0} mm = {1:0.######} units
+{2:0.####} m = {3:0.#####} units 
+{4:0.####}' =  {5:0.######} units 
+{6:0.####}'' = {7:0.#####} units", cm*10, unit*10, cm/100, unit * 10, inch, inch/0.75, foot, foot*16);
             }
             else
             {
@@ -50,6 +55,11 @@ namespace HammerTools
             {
                 label2.Text = "Wrong param!";
             }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://steamcommunity.com/id/bawdyq/");
         }
     }
 }
